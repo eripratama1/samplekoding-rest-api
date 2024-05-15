5debb1db83602ab75cea1ca30cf51d42c2fcfc9b
eripratama1 <eripratama90@gmail.com>
2024-05-15 22:53:36 +0800

    create endpoint insert data

--- app/Http/Controllers/API/v1/ArticleController.php
+++ app/Http/Controllers/API/v1/ArticleController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Article;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
 
 class ArticleController extends Controller
 {
@@ -36,6 +41,49 @@ class ArticleController extends Controller
                 'message' => 'List articles',
                 'status' => Response::HTTP_OK
             ], Response::HTTP_OK);
+        }
+    }
+
+    public function store(Request $request)
+    {
+
+        /**
+         * Melakukan validasi menggunakan validator
+         */
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+            'publish_date' => 'required'
+        ]);
+
+        /** Jika ada data yang tidak valid return error response berikut */
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        try {
+            /** Jika validasi berhasil lakukan proses simpan data lalu return
+             *  response JSON dibawah
+             */
+            Article::create([
+                'title' => $request->title,
+                'content' => $request->content,
+                'publish_date' => Carbon::create($request->publish_date)->toDateString(),
+            ]);
+
+            return response()->json([
+                'status' => Response::HTTP_OK,
+                'message' => 'Data stored to db'
+            ], Response::HTTP_OK);
+        } catch (Exception $e) {
+
+            /** Jika proses simpan data gagal return response JSON berikut */
+            Log::error('Error storing data :' .  $e->getMessage());
+
+            return response()->json([
+                'status' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => 'Failed stored data to db'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,5 @@ Route::get('/user', function (Request $request) {
 /** Menambahkan prefix v1 pada route api (http://localhost:8000/api/v1/route_yang_diakses) */
 Route::prefix('v1')->group(function(){
     Route::get('list-articles',[App\Http\Controllers\API\v1\ArticleController::class,'index']);
+    Route::post('store-article',[App\Http\Controllers\API\v1\ArticleController::class,'store'])->middleware('api');
 });
